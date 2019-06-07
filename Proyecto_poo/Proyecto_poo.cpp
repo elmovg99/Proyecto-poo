5a4aa728f6c69fcf9a8a8b7e3258305cda4663df
Elmo <elmovg_99@hotmail.com>
2019-06-06 23:40:54 -0500

    Inicio del codigo
    Pongase a trabajar

---
+++ Proyecto_poo/Proyecto_poo.cpp
@@ -0,0 +1,15 @@
+// Proyecto_poo.cpp: define el punto de entrada de la aplicación de consola.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "hola mundo" << endl;
+
+    return 0;
+}
+
